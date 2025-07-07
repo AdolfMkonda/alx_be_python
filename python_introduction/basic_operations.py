80422b943d47edc7a5b2aeb1ab1a95165ccbb57a
AdolfMkonda <adolfanatory@gmail.com>
2025-07-07 21:23:42 +0300

    Adding directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+# A python program that performs basic arithmetic operations
+#
+#
+number1 = 10
+number2 = 15
+print("Sum"," of ",number1," and ",number2," is ",number1 + number2)
+print("Substraction"," of ",number2," and ",number1," is ",number2 - number1)
+print("Product"," of ",number1," and ",number2," is ",number1 * number2)
