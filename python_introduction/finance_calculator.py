8bfe9db5189053fdde7cedabacdaca8e91ebf7ab
AdolfMkonda <adolfanatory@gmail.com>
2025-07-07 22:33:19 +0300

    fixing bracket error

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,7 @@
 # A program That calculate and provide feedback on a user’s monthly savings and potential future savings without applying conditional statements.
 monthlyIncome = int(input("Enter your monthly income: "))
 monthlyExpenses = int(input("Enter your total monthly expenses: "))
-ms = float(monthlyExpenses) - float(monthlyIncome)
-print("Your monthly savings are ",ms)
-print("Projected savings after one year, with interest, is: ",ms * 12 + (ms * 12 *0.05))
+ms = monthlyExpenses - monthlyIncome
+pr = ms * 12 + (ms * 12 * 0.05)
+print("Your monthly savings are $",ms)
+print("Projected savings after one year, with interest, is: ",pr)
