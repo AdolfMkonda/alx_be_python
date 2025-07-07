ad03acd27636d2621a9e13b33e1c9daa735b9c01
AdolfMkonda <adolfanatory@gmail.com>
2025-07-07 21:27:31 +0300

    Adding directory

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,7 +2,7 @@
 #
 #
 number1 = 10
-number2 = 15
+number2 = 5
 print("Sum"," of ",number1," and ",number2," is ",number1 + number2)
 print("Substraction"," of ",number2," and ",number1," is ",number2 - number1)
 print("Product"," of ",number1," and ",number2," is ",number1 * number2)
