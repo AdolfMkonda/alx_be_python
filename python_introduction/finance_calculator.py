901511c231be4212ac8d55ff963b91a90961eb6b
AdolfMkonda <122844249+AdolfMkonda@users.noreply.github.com>
2025-07-09 12:19:41 +0300

    fixing error

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,7 +2,7 @@
 # without applying conditional statements.
 monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = monthly_expenses - monthly_income
+monthly_savings = monthly_income - monthly_expenses
 projected_saving = float(monthly_savings) * 12 + float(monthly_savings) * 12 * 0.05
 print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected savings after one year, with interest, is: ${projected_saving}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_saving)}.")
