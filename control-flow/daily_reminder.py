9d1c712210245ff14cc583598f6c6944cc9a9c30
AdolfMkonda <adolfanatory@gmail.com>
2025-07-19 20:28:54 +0300

    fixing errors

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,23 +4,18 @@
 # relying on data structures to store multiple tasks.
 
 task = input("Enter your task: ")
-
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
-        if time_bound == "yes":
-            print(f"'{task}' is a high priority task that requires immediate attention today!")
-        else:
-            print(f"'{task}' is a high priority task. Consider completing it when you have free time.")
+        print(f"'{task}' is a high priority task",end="")
     case "medium":
-        if time_bound == "no":
-            print(f"'{task}' is a medium priority task. Consider completing it when you have free time.")
-        else:
-            print(f"'{task}' is a medium priority task that requires immediate attention today!")
+        print(f"'{task}' is a medium priority task",end="")
     case "low":
-        if time_bound == "no":
-            print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
-        else:
-            print(f"'{task}' is a low priority task that requires immediate attention today!")
+        print(f"'{task}' is a low priority task",end="")
+
+if time_bound == "yes":
+    print(" that requires immediate attention today!")
+else:
+    print(". Consider completing it when you have free time.")
