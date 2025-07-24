5bcabcdbf75cf79f4abd33a9d4014341b1226e2f
AdolfMkonda <adolfanatory@gmail.com>
2025-07-24 14:37:01 +0300

    output format fixed

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
     return datetime.datetime.now("%Y-%m-%d %H:%M:%S")
 
 current_date = display_current_datetime()
-print(current_date)
+print(f"Current date and time: {current_date}")
 
 days = int(input("Enter the number of days to add to the current date: "))
 
