d17d6ff879b3e1614362416e0457df0ec1d023cf
AdolfMkonda <adolfanatory@gmail.com>
2025-07-24 21:12:17 +0300

    added global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,8 +4,8 @@
 # that are accessible within functions.
 
 
-global FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-global CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+global FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+global CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
