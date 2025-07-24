f1bb353bc8114e0183c3597ef1a5b2113d8ff300
AdolfMkonda <adolfanatory@gmail.com>
2025-07-25 01:46:22 +0300

    error handling

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -17,6 +17,7 @@ try:
    temperature = float(temperature)
 except ValueError:
     print("nvalid temperature. Please enter a numeric value.")
+    exit()
 format = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
 if format == "F":
