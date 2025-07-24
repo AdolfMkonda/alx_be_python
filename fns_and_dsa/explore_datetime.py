e672a314123013739fbe2d32e74a8d87ae0da580
AdolfMkonda <adolfanatory@gmail.com>
2025-07-24 18:48:07 +0300

    fixing output format

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -13,8 +13,7 @@ print(f"Current date and time: {current_date}")
 days = int(input("Enter the number of days to add to the current date: "))
 
 def calculate_future_date(days):
-    change = timedelta(days=days)
-    return current_date + change
+    return current_date + timedelta(days=days)
 
 future_date = calculate_future_date(days)
-print(future_date.date)
+print(future_date.strftime("%Y-%m-%d"))
