0d87df740d7eb3b2794860ec420fd7ab8f1840d2
AdolfMkonda <adolfanatory@gmail.com>
2025-08-02 17:04:20 +0300

    fixing burgs

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -22,7 +22,8 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        self.account_balance = self.account_balance
+        return f"Current Balance: ${self.account_balance}"
         
 
         
