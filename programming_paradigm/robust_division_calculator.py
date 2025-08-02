c3a4d66684d6405b877beb90ff13b48183de7724
AdolfMkonda <adolfanatory@gmail.com>
2025-08-02 13:50:53 +0300

    Added rubust calculatro

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+# Implement a division calculator that robustly handles errors like division 
+# by zero and non-numeric 
+# inputs using command line arguments.
+
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        numerator / denominator
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+    
