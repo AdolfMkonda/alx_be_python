40935a8881f649a6eb9308f55162a72d66aac287
AdolfMkonda <adolfanatory@gmail.com>
2025-08-02 16:11:19 +0300

    added lib

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,28 @@
+# Solidify understanding of basic OOP concepts in 
+# Python by implementing a system that tracks books in a library, 
+# focusing on classes, object 
+# instantiation, and method invocation.
+
+
+class Book:
+    def __init__(self, title, author, _is_checked_out):
+        self.title = title
+        self.author = author
+        self.___is_checked_out = _is_checked_out
+
+class Library(Book):
+    def __init__(self, _books, ):
+        pass
+
+    def add_book(self, title, author):
+        self.title = title
+        self.author = author
+
+    def check_out_book(self, title):
+        self.title = title
+
+    def return_book(self, title):
+        self.title = title
+
+    def list_available_books(self):
+        pass
