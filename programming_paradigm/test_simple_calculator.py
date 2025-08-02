e933b07f99e358a148fbd644b3cf4dee1027cffa
AdolfMkonda <adolfanatory@gmail.com>
2025-08-02 15:47:11 +0300

    added correct fx name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,7 +20,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 1), 4)
         self.assertEqual(self.calc.subtract(-1, 1), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5, 1), 5)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
 
