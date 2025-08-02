42189644c01020b5b7e3af10a36eead834e8ec3d
AdolfMkonda <adolfanatory@gmail.com>
2025-08-02 17:14:43 +0300

    fixing burgs

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,6 +17,7 @@ class BankAccount:
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
+            return True
         else:
             return False
     
