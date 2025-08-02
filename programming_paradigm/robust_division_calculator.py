55c703b7416c025a2f4d06c6fec5435141f1d123
AdolfMkonda <adolfanatory@gmail.com>
2025-08-02 13:58:02 +0300

    Added multiple error handling

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,13 +5,19 @@
 def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
+    
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+    
+    try:
         denominator = float(denominator)
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        
+    try:
         result = numerator / denominator
         return f"The result of the division is {result}"
-    except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
-    except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+    
     except ValueError:
         print("Error: Please enter numeric values only.")
     
