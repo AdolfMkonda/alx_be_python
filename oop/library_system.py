27ffd5459a5a507a43f5c4a0568989742d5e8a88
AdolfMkonda <adolfanatory@gmail.com>
2025-08-08 16:38:41 +0300

    added methods in book and library class

---
+++ oop/library_system.py
@@ -0,0 +1,29 @@
+# Deepen your understanding of inheritance 
+# and composition in Python by creating a system 
+# that models a library with different types of books.
+
+class Book:
+    books = []
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, file_size):
+        pass
+
+
+class PrintBook(Book):
+    def __init__(self, page_count):
+        self.page_count = page_count
+
+class Library(Book, EBook, PrintBook):
+    def __init__(self, title, author):
+        pass
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        return f"PrintBook: {self.title} {self.author}, Page Count: {self.page_count}"
