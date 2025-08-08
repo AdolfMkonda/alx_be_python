df7cfee2d3358ec13edb09e82c13de98cc30c954
AdolfMkonda <adolfanatory@gmail.com>
2025-08-08 17:33:26 +0300

    Added polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+# Enhance your understanding of polymorphism 
+# in Python by creating a set of classes 
+# that demonstrate method overriding and polymorphic behavior.
+
+import math
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        super().__init__()
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        super().__init__()
+        return math.pi * self.radius * self.radius
+        
